7905d1159c259a1bbe50c7c5d35218e836f4e3a5
Laura McCool <mccool_laura@yahoo.com>
2012-06-28 20:34:21 -0500

    d

---
+++ X-Mas Android/assets/www/js/json.js
@@ -0,0 +1,30 @@
+var json = {
+	"gift1": {
+		"category":["Category:", "Electronics"], 
+		"comments":["Gift Description:", "Digital Camera"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-12-01"]
+		},
+	"gift2": {
+		"category":["Category:", "Entertainment"], 
+		"comments":["Gift Description:", "Bluray"],
+		"amount":["Quantity:","1"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Amazon"],
+		"url":["Product Url:","amazon.com"],
+		"date":["Date Added:","2011-11-27"]
+		},
+	"gift3": {
+		"category":["Category:", "Apparel"], 
+		"comments":["Gift Description:", "Jeans"],
+		"amount":["Quantity:","2"],
+		"location":["Where To Buy:","Online"],
+		"store":["Store Name:","Delias"],
+		"url":["Product Url:","delias.com"],
+		"date":["Date Added:","2011-11-30"]
+		}
+	}
+
